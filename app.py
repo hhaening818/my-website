22a7bd90e191a32e718c152e5866dec4ff424c54
hhaening818 <hhaening818@users.noreply.github.com>
2026-02-22 19:35:15 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1>🚀 배포 성공!</h1>"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
